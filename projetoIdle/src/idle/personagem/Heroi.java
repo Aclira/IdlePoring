cd42099194914155b0068ca7d595b3799de5d37c
Jose de Senna <jose.de.senna@hotmail.com>
2018-11-15 17:52:35 -0300

    Correcao dos metodos getters e setters e correcao da chamada de batalha

--- projetoIdle/src/idle/personagem/Heroi.java
+++ projetoIdle/src/idle/personagem/Heroi.java
@@ -2,101 +2,43 @@ package idle.personagem;
 import idle.elementos.Classe;
 
 public class Heroi extends Personagem{
-    private int exp, poder, dest, agil, sorte, intelig, critico;
+    private int exp;
     private Classe classe;
 
     public Heroi(Classe classe){
         this.classe = classe;
         this.exp = 0;
-        this.poder = 0;
-        this.dest = 0;
-        this.agil = 0;
-        this.sorte = 0;
-        this.intelig = 0;
     }
 
     public Heroi(String nome,Classe classe){
         super(nome);
         this.classe = classe;
         this.exp = 0;
-        this.poder = 0;
-        this.dest = 0;
-        this.agil = 0;
-        this.sorte = 0;
-        this.intelig = 0;
     }
        
 
     public int getExp() {
-        return exp;
+        return this.exp;
     }
 
-    //Todos setters com condição de atribuição
     public void setExp(int exp) {
-        if(this.limiteBase(exp))
-            this.exp = exp;
+        this.exp = exp;
     }
-
-    public int getPoder() {
-        return poder;
-    }
-
-    public void setPoder(int poder) {
-        if(this.limiteBase(poder))
-            this.poder = poder;
-    }
-
-    public int getDest() {
-        return dest;
-    }
-
-    public void setDest(int dest) {
-        if(this.limiteBase(dest))
-            this.dest = dest;
-    }
-
-    public int getAgil() {
-        return agil;
-    }
-
-    public void setAgil(int agil) {
-        if(this.limiteBase(agil))
-            this.agil = agil;
-    }
-
-    public int getSorte() {
-        return sorte;
-    }
-
-    public void setSorte(int sorte) {
-        if(this.limiteBase(sorte))
-            this.sorte = sorte;
-    }
-
-    public int getIntelig() {
-        return intelig;
-    }
-
-    public void setIntelig(int intelig) {
-        if(this.limiteBase(intelig))
-            this.intelig = intelig;
-    }
-
 
     public Classe getClasse() {
-        return classe;
+        return this.classe;
     }
 
     public void setClasse(Classe classe) {
         this.classe = classe;
     }
     
-    //Ação ataque do heroi definido como método
-    public void ataque(Heroi heroi, Inimigo inimigo){
-        heroi.hp = heroi.hp - (inimigo.getAtq() - heroi.getDefesa());    
-        //heroi.setHp(heroi.getDefesa() - inimigo.getAtq());
-            
-            System.out.println("Hp do heroi " + heroi.getHp());
-        }
+    // Ação de batalha do herói
     
+    public void batalha(Inimigo inimigo, Heroi heroi){
+        int dano = (inimigo.getAtaque()*inimigo.getInteligencia() + inimigo.getSorte())/heroi.getDefesa();
+        int hp = heroi.getHp() - dano;
+        heroi.setHp(hp); 
+        System.out.println("Hp do herói " + heroi.getHp());
+    }
 }
