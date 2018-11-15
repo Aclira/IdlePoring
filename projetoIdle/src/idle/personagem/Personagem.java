e986bab575941914fa0029f6bfe09d9faceb8e68
Jose de Senna <jose.de.senna@hotmail.com>
2018-11-15 17:53:54 -0300

    Mudanca de atributos de personagem

--- projetoIdle/src/idle/personagem/Personagem.java
+++ projetoIdle/src/idle/personagem/Personagem.java
@@ -1,125 +1,52 @@
 package idle.personagem;
 
 public abstract class Personagem {
-        //Definição de variáveis privadas para que só sejam alteradas por meio 
-        //de métodos
-	private String nome;
-	private int nivel, defesa, esquiva, atq, atqsp;
-        //Var hp public para que seja possivel usar diretamente no metodo ataque
-        //em heroi e inimigo
-        public int hp;
-        //Definição de valores limites que não podem ser modificados (final)
-        //para os atributos da classe
-	public static final int HP_MAX = 100000;
-	public static final int BASE_MAX = 1000;
-	public static final int NIVEL_MAX = 100;
+    
+    // Atributos de personagem: alterados apenas através de métodos
+	
+    private String nome;
+	
+    private int nivel;
+    private int hp;
+	
+    private int ataque;
+    private int defesa;
+    private int inteligencia;
+    private int sorte;
+	
+    // Valores limite que não podem ser modificados (finais) para os atributos da classe
+	
+    public static final int NIVEL_MAX = 10;
 
-        //Definido construtor vazio
-	public Personagem() {
+    // Construtor vazio: utilizado em caso de demonstração de criação de novo personagem
+	
+    public Personagem() {
+        this.nome = "";
+		
+	this.nivel = 1;
+	this.hp = nivel*1000;
+		
+	this.ataque = nivel*100;
+	this.defesa = nivel*100;
+	this.inteligencia = nivel*10;
+	this.sorte = 0;	// Definido como 0 por que deve receber um valor aleatório calculado na hora da criação
+}
+    
+    // Construtor com parâmetro "nome": exemplo de sobrecarga de métodos
+    
+    public Personagem(String nome) {
 		this.nome = "";
-		this.hp = 0;
-		this.nivel = 0;
-		this.defesa = 0;
-		this.esquiva = 0;
-		this.atq = 0;
-		this.atqsp = 0;	
-	}
-        //Definido outro construtor, mas que recebe 1 parametro apenas.
-        //Exxemplo de sobre carga de métodos, porque já tem um construtor declarado.
-	public Personagem(String nome) {
-		this.nome = nome;
-		this.hp = 0;
-		this.nivel = 0;
-		this.defesa = 0;
-		this.esquiva = 0;
-		this.atq = 0;
-		this.atqsp = 0;	
-	}	
-        
-        //A partir daqui são definidos os métodos getters e setters, por conta
-        //das variáveis privadas
-	public String getNome() {
-		return this.nome;
+		
+		this.nivel = 1;
+		this.hp = nivel*1000;
+		
+		this.ataque = nivel*100;
+		this.defesa = nivel*100;
+		this.inteligencia = nivel*10;
+		this.sorte = 0;	// Definido como 0 por que deve receber um valor aleatório calculado na hora da criação	
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-
-	public int getHp() {
-		return this.hp;
-	}
-        
-        //Os métodos responsáveis por "enviar" valores para as variáveis estão
-        //dentro da condição de valores estabelecidos nas variaveis de classe(static final)
-	public void setHp(int hp) {
-                //O método limiteHP está declarado logo abaixo
-		if(this.limiteHP(hp))
-                        //Se a condição for verdadeira, então o atributo recebe o valor.
-			this.hp = hp;
-	}
-        
-        //O mesmo acontece nos setters seguintes.
-        
-	public int getNivel() {
-		return this.nivel;
-	}
-        
-	public void setNivel(int nivel) {
-		if(this.limiteNivel(nivel))
-			this.nivel = nivel;
-	}
-
-	public int getDefesa() {
-		return this.defesa;
-	}
-
-	public void setDefesa(int defesa) {
-		if(this.limiteBase(defesa))
-			this.defesa = defesa;
-	}
-
-	public int getEsquiva() {
-		return this.esquiva;
-	}
-
-	public void setEsquiva(int esquiva) {
-		if(this.limiteBase(esquiva))
-			this.esquiva = esquiva;
-	}
-
-	public int getAtq() {
-		return this.atq;
-	}
-
-	public void setAtq(int atq) {
-		if(this.limiteBase(atq))
-			this.atq = atq;
-	}
-
-	public int getAtqSp() {
-		return this.atqsp;
-	}
-
-	public void setAtqSp(int atqsp) {
-		if(this.limiteBase(atqsp))
-			this.atqsp = atqsp;
-	}
-        
-	public boolean limiteHP(int valor) {
-		if(valor >= 0 && valor <= this.HP_MAX)
-			return true;
-		else
-			return false;
-	}
-
-	public boolean limiteBase(int valor) {
-		if(valor >= 0 && valor <= this.BASE_MAX)
-			return true;
-		else
-			return false;
-	}
+	// Método limiteNivel: responsável por verificar se o valor atribuído à variável nível é válido
 
 	public boolean limiteNivel(int valor) {
 		if(valor >= 0 && valor <= this.NIVEL_MAX)
@@ -127,19 +54,90 @@ public abstract class Personagem {
 		else
 			return false;
 	}
+    
+    // Definição de getters e setters em função das variáveis privadas
+    
+    // Atributo nível
+    
+    public int getNivel() {
+    	return this.nivel;
+    }
+    
+    public void setNivel(int nivel) {
+    	if(this.limiteNivel(nivel))
+            this.nivel = nivel; // Receberá o valor atribuído pelo usuário apenas se a condição for verdadeira
+    	else
+            this.nivel = NIVEL_MAX; // Caso contrário, receberá o máximo permitido
+    }
+    
+    // Atributo nome
+     
+    public String getNome() {
+    	return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+		
+    // Atributo hp
+
+    public int getHp() {
+	return this.hp;
+    }
         
-        //Mudança de comportamento no método toString(é chamado ao printar).
-        //Aqui foi decidido a forma que ele deve se comportar ao printar.
-        //Exemplo de polimorfismo
-        @Override
-        public String toString() {
-            String print = this.getNome().toUpperCase() + "\n";
-            print += "NÍVEL: " + this.getNivel() + "\n";
-            print += "HP: " + this.getHp() + "\n";
-            print += "ATQ: " + this.getAtq() + "\n";
-            print += "DEF: " + this.getDefesa() + "\n";
-            print += "ATQ SP: " + this.getAtqSp() + "\n";
-            print += "ESQUIVA: " + this.getEsquiva() + "\n";
-            return print;
-        }
+    public void setHp(int hp) {
+	this.hp = this.hp + hp;
+    }
+	
+    // Atributo ataque
+	
+    public int getAtaque() {
+	return this.ataque;
+    }
+		
+    public void setAtaque(int ataque) {
+	this.ataque = ataque;
+    }
+		
+    // Atributo defesa
+
+    public int getDefesa() {
+	return this.defesa;
+    }
+
+    public void setDefesa(int defesa) {
+	this.defesa = defesa;
+    }
+		
+    // Atributo inteligencia
+		
+    public int getInteligencia() {
+        return this.inteligencia;
+    }
+
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+    
+    // Atributo sorte
+    
+    public int getSorte() {
+        return this.sorte;
+    }
+       
+    /* Mudança de comportamento no método toString (é chamado ao printar). Aqui foi decidido a forma que ele deve
+    se comportar ao printar. Exemplo de polimorfismo. */
+ 
+    @Override
+    public String toString() {
+        String print = this.getNome().toUpperCase() + "\n";
+        print += "NÍVEL: " + this.getNivel() + "\n";
+        print += "HP: " + this.getHp() + "\n";
+        print += "ATAQUE: " + this.getAtaque() + "\n";
+        print += "DEFESA: " + this.getDefesa() + "\n";
+        print += "INTELIGÊNCIA: " + this.getInteligencia() + "\n";
+        print += "SORTE: " + this.sorte + "\n";           
+        return print;
+    }
 }
