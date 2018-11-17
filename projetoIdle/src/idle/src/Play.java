7ddb896c50367ea8540ba759e7ec12e70dcfe2e3
Jose de Senna <jose.de.senna@hotmail.com>
2018-11-17 09:37:54 -0300

    Atualizando o sistema inicial para as novas profissoes

--- projetoIdle/src/idle/src/Play.java
+++ projetoIdle/src/idle/src/Play.java
@@ -1,15 +1,21 @@
 package idle.src;
 
 import idle.elementos.Batalha;
-import idle.elementos.Classe;
+
+import idle.elementos.profissoes.Arqueiro;
+import idle.elementos.profissoes.Guerreiro;
+import idle.elementos.profissoes.Mago;
+
 import idle.elementos.Raca;
+
 import idle.personagem.Heroi;
 import idle.personagem.Inimigo;
 
 public class Play {
     public static void main(String args[]){
-	Classe mago = new Classe("Mago");
-        Raca retardado = new Raca("Arqueiro");
+	Mago mago = new Mago();
+        Raca retardado = new Raca("Retardado");
+        
         Heroi heroi = new Heroi("Acza", mago);
         Inimigo inimigo = new Inimigo("Andre", retardado);
         
